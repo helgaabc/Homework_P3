f1e7182b6863bea8c084fde5840d9814e4507cf3
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-09-14 10:34:02 +0300

    task 19

---
+++ Task 19/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: N ");
+string a = Console.ReadLine(); 
+
+if (a.Length !=5)
+{
+    Console.WriteLine("Число не является палиндромом");
+}
+else
+{
+    if (a[0] == a[4] && a[1] == a[3])
+    {
+       Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндромом");
+    }
+}
