c22f9ab1b94ee77369b382e8d676425ab49f198d
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-09-15 10:01:24 +0300

    added task 23

---
+++ Task 23/Program.cs
@@ -0,0 +1,12 @@
+﻿void Searching (int a) 
+{    
+     int b = 1; 
+    while (b <= a)  
+   {        
+     Console.WriteLine(Math.Pow(b, 3));  
+            b = b+1;    
+   } 
+} 
+
+Searching(3);
+Searching(5);
