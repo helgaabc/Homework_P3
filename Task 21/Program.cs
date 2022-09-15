82dd8cd371cf18e89937044b2ebd93b96bf321a5
elena.bondarchuk <e-bondarchuk@mail.ru>
2022-09-15 09:54:08 +0300

    added task 21

---
+++ Task 21/Program.cs
@@ -0,0 +1,6 @@
+﻿double Distance (double x1, double y1, double z1, double x2, double y2, double z2)
+{    
+     return Math.Round(Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2)+  Math.Pow(z1 - z2,2)),4); 
+}  
+Console.WriteLine (Distance(3, 6, 8, 2, 1, -7));
+Console.WriteLine (Distance(7, -5, 0, 1, -1, 9));
